d25bfab2f876e3033c4461e65056e262c29448fa
ebella <ebella@student.42.fr>
2026-01-22 13:37:13 +0100

    doing cpp05 ex02

---
+++ cpp05/ex02/AForm.hpp
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AForm.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebella <ebella@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/20 14:08:27 by ebella            #+#    #+#             */
+/*   Updated: 2026/01/21 11:46:13 by ebella           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AForm_HPP
+#define AForm_HPP
+
+#include <string>
+#include <iostream>
+#include <exception>
+
+class Bureaucrat;
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+#define ORANGE "\033[38;5;208m"
+
+class AForm
+{
+	private:
+		std::string const name;
+		bool isSigned;
+		const int gradeToSign;
+		const int gradeToExecute;
+		
+		static const int highestGrade = 1;
+		static const int lowestGrade = 150;
+	
+	public:
+		AForm();
+		AForm(std::string const &name, int gradeToSign, int gradeToExecute);
+		AForm(const AForm& other);
+		AForm& operator=(const AForm& other);
+		~AForm();
+		
+		std::string getName() const;
+		bool getIsSigned() const;
+		int getGradeToSign() const;
+		int getGradeToExecute() const;
+		
+		void beSigned(Bureaucrat& bureaucrat);
+		
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+		
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+};
+
+std::ostream& operator<<(std::ostream& out, const AForm& AForm);
+
+#endif
